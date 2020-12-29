853a291b6b0c2101479ea7ac593bfbb0b98af729
MayronSouza <mayron.souza@gmz.solutions>
2020-12-29 20:05:31 -0300

    Construindo a estrutura padrao para o App

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,20 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Buscador de GIFs',
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
