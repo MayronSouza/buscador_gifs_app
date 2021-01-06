15b09e724b58002d1146dca3a77bbb943063f49d
MayronSouza <mayron.souza@gmz.solutions>
2021-01-06 16:06:18 -0300

    Utilizando GridView para exibir uma tabela com os gifs vindos da API do GIPHY

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -81,7 +81,7 @@ class _HomePageState extends State<HomePage> {
                     if (snapshot.hasError)
                       return Container();
                     else
-                      _createGifTable(context, snapshot);
+                      return _createGifTable(context, snapshot);
                 }
               },
             ),
@@ -91,5 +91,24 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  _createGifTable(BuildContext context, AsyncSnapshot snapshot) {}
+  _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      padding: EdgeInsets.all(10.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
+      ),
+      itemCount: snapshot.data['data'].length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          child: Image.network(
+            snapshot.data['data'][index]['images']['fixed_height']['url'],
+            height: 300.0,
+            fit: BoxFit.cover,
+          ),
+        );
+      },
+    );
+  }
 }
