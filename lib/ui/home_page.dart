f6e8e5c9fb2dc7c1d8edfab01aa2172aaa1708ad
MayronSouza <mayron.souza@gmz.solutions>
2021-01-07 17:08:38 -0300

    Fazendo com que os gifs nao apareçam de forma abrupta na tela, com o transpatent_image aparecera como fade-in

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 
 import 'package:buscador_gifs_app/ui/gif_page.dart';
 import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -119,8 +120,10 @@ class _HomePageState extends State<HomePage> {
       itemBuilder: (context, index) {
         if (_search == null || index < snapshot.data['data'].length) {
           return GestureDetector(
-            child: Image.network(
-              snapshot.data['data'][index]['images']['fixed_height']['url'],
+            child: FadeInImage.memoryNetwork(
+              placeholder: kTransparentImage,
+              image: snapshot.data['data'][index]['images']['fixed_height']
+                  ['url'],
               height: 300.0,
               fit: BoxFit.cover,
             ),
